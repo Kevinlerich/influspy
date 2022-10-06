a119876e3c6afd4022f5723da9072eec8c26f6cc
kevin ngankou <kevinngankou@macbook-pro-de-kevin.local>
2022-10-06 21:02:30 +0200

    first commit

---
+++ src/main/java/com/example/influspy/InfluspyApplication.java
@@ -0,0 +1,13 @@
+package com.example.influspy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluspyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfluspyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/influspy/InfluspyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.influspy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluspyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
